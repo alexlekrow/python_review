631535273fb384fbd49c1485fe99f41dcc30cc57
Alex K. Austin <akac92@gmail.com>
2020-08-16 10:56:27 -0700

    Complete examples for formatting prints 1.2.3

--- rust/rust_by_example/hello_world/src/main.rs
+++ rust/rust_by_example/hello_world/src/main.rs
@@ -1,11 +1,13 @@
 mod formatted_print;    // 1.2
 mod debug_print;        // 1.2.1
 mod display_print;      // 1.2.2
-mod test_case;      // 1.2.2.1
+mod test_case;          // 1.2.2.1
+mod formatting_print;   // 1.2.3
 
 fn main() {
     // formatted_print::run();
     // debug_print:run();
     // display_print::run();
-    test_case::run();
+    // test_case::run();
+    formatting_print::run();
 }
