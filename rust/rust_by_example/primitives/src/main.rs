67c5154e055cc830535994730738838cfc1f9efe
Alex K. Austin <akac92@gmail.com>
2020-08-16 22:34:50 -0700

    Add arrays and slices code samples

--- rust/rust_by_example/primitives/src/main.rs
+++ rust/rust_by_example/primitives/src/main.rs
@@ -1,8 +1,10 @@
-mod literal_operators;    // 2.1
-mod tuples;    // 2.2
+mod literal_operators;      // 2.1
+mod tuples;                 // 2.2
+mod arrays_slices;          // 2.2
 
 
 fn main() {
     // literal_operators::run();
-    tuples::run();
+    // tuples::run();
+    arrays_slices::run();
 }
