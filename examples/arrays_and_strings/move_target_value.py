a9d9c6b88c295cb2722f91ddce8e2c2bcb917c61
Alex Austin <akac92@gmail.com>
2021-12-24 23:27:31 -0800

    Finish documentation for array problem

--- examples/arrays_and_strings/move_target_value.py
+++ examples/arrays_and_strings/move_target_value.py
@@ -1,32 +1,39 @@
 ''' 
 Common Problem Description:
-clear
-
+Move all Zeros (target) to the left (or right) of an array inplace 
+while maintaining ordering of non-target values.
 
 Example Input:
-[1, 10, 20, 0, 58, 63, 0, 88, 0]
+arr = [1, 10, 20, 0, 58, 63, 0, 88, 0]
+target = 0
 
 Expected Output:
 [0, 0, 0, 1, 10, 20, 58, 63, 88]
 '''
 
 
-def move_target_to_right(A, target):
-    reserved = 0
-    for i in range(len(A)):
-        if A[i] == target:
-            reserved += 1
-        elif reserved > 0:
-            A[i], A[i - reserved] = A[i - reserved], A[i]
+def swap(arr, swapA, swapB):
+    arr[swapA], arr[swapB] = arr[swapB], arr[swapA]
 
 
-def move_target_to_left(A, target):
+def move_target_to_right(arr, target):
+    N = len(arr)
     reserved = 0
-    for i in reversed(range(len(A))):
-        if A[i] == target:
+    for i in range(N):
+        if arr[i] == target:
             reserved += 1
         elif reserved > 0:
-            A[i], A[i + reserved] = A[i + reserved], A[i]
+            swap(arr, i, i - reserved)
+
+
+def move_target_to_left(arr, target):
+    N = len(arr)
+    reserved = 0
+    for i in reversed(range(N)):
+        if arr[i] == target:
+            reserved += 1
+        elif reserved > 0:
+            swap(arr, i, i + reserved)
 
 
 def driver():
