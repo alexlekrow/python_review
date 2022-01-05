95021d1d3d17a7e15ce47ef4ad8a4d0496e92975
Alex Austin <akac92@gmail.com>
2022-01-04 22:13:05 -0800

    Clean up old comments

--- examples/sqlmodel_demo/app.py
+++ examples/sqlmodel_demo/app.py
@@ -1,6 +1,3 @@
-#####################################
-# Multi-file structure
-
 from db import engine
 from sqlmodel import SQLModel
 
@@ -13,24 +10,3 @@ def create_db_and_tables():
 
 if __name__ == "__main__":
     create_db_and_tables()
-
-#####################################
-# Single file structure
-# from typing import Optional
-
-# from sqlmodel import Field, SQLModel, create_engine
-
-
-# class Hero(SQLModel, table=True):
-#     id: Optional[int] = Field(default=None, primary_key=True)
-#     name: str
-#     secret_name: str
-#     age: Optional[int] = None
-
-
-# sqlite_file_name = "database.db"
-# sqlite_url = f"sqlite:///{sqlite_file_name}"
-
-# engine = create_engine(sqlite_url, echo=True)
-
-# SQLModel.metadata.create_all(engine)
