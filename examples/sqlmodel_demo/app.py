75444b83c4a54cf7333d9b88c6f19cc1313448a5
Alex Austin <akac92@gmail.com>
2022-01-23 11:23:17 -0800

    Add more records to the DB

--- examples/sqlmodel_demo/app.py
+++ examples/sqlmodel_demo/app.py
@@ -1,12 +1,47 @@
 from db import engine
-from sqlmodel import SQLModel
+from sqlmodel import Session, SQLModel, col, or_, select
 
 from models import *
 
 
 def create_db_and_tables():
+    '''Initialize SQLModel ORM using imported  db engine'''
     SQLModel.metadata.create_all(engine)
 
 
-if __name__ == "__main__":
+def create_heroes():
+    '''Add Hero instances to memory and commit through a session transaction.'''
+    heroes = [
+        Hero(name="Deadpond", secret_name="Dive Wilson", age=40),
+        Hero(name="Spider-Boy", secret_name="Pedro Parqueador", age=25),
+        Hero(name="Rusty-Man", secret_name="Tommy Sharp", age=48),
+        Hero(name="Tarantula", secret_name="Natalia Roman-on", age=32),
+        Hero(name="Black Lion", secret_name="Trevor Challa", age=35),
+        Hero(name="Dr. Weird", secret_name="Steve Weird", age=36),
+        Hero(name="Captain NA", secret_name="Esteban Rogelios", age=93)
+    ]
+
+    with Session(engine) as session:
+        for hero in heroes:
+            session.add(hero)
+        session.commit()
+
+
+def select_heroes():
+    with Session(engine) as session:
+        heroes = session.exec(select(Hero)
+                              .where(or_(col(Hero.name) == "Black Lion", col(Hero.age) == 25))
+                              #   .where(Hero.age == 48)
+                              ).all()
+        print(heroes)
+
+
+def main():
+    '''Creates required databases and ORM components.'''
     create_db_and_tables()
+    create_heroes()
+    select_heroes()
+
+
+if __name__ == "__main__":
+    main()
